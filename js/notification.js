02f9ab615c9f507a8bc558e17f1abce624934db7
hackmineatualizado <133298220+hackmineatualizado@users.noreply.github.com>
2023-05-26 21:46:59 -0300

    Create notification.js

---
+++ js/notification.js
@@ -0,0 +1,19 @@
+const container = document.querySelector(".notification");
+function createNotification() {
+  const notification = document.createElement("div");
+  notification.classList.add("toast");
+  notification.innerHTML = `Promoção próxima do fim. Inscreva-se agora!!
+  <a href="https://www.betsury.com/?p=UU7gxMgM&lang=pt" target="_blank"
+        ><button id="cadastro" class="cadastro">Cadastre-se Aqui</button></a
+      >
+  `;
+
+  container.appendChild(notification);
+  setTimeout(() => {
+    notification.remove();
+  }, 5000);
+}
+
+setTimeout(() => {
+  createNotification();
+}, 5000);
